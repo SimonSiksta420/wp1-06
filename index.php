7be949306e09967292932b9b59d6608754955aa4
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-03-31 09:29:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$woodchunk = true;
+$rope = true;
+$nails = true;
+$knife = true;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if ($rope) {
+    echo = "Získal jsi lano.";
+}
+if ($woodchunk) {
+    echo = "Získal jsi kus dřeva";
+}
+if ($nails) {
+    echo = "Získal jsi hřebíky";
+}
+if ($knife) {
+    echo = "Získal jsi nůž";
+}
+    
+</body>
+</html>
